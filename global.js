9d973a4cb25ce4d40eee8604916eb31eaf1b9f16
Arthur Fiorette <me@arthur.place>
2024-05-06 22:40:01 -0300

    refactor: removed __ from funtion name

--- global.js
+++ global.js
@@ -1,8 +1,8 @@
-function __tupleResolve(value) {
+function tupleResolve(value) {
   return [null, value]
 }
 
-function __tupleReject(error) {
+function tupleReject(error) {
   if (!(error instanceof Error)) {
     return [new TupleItError(error)]
   }
@@ -14,6 +14,6 @@ Object.defineProperty(Promise.prototype, 'tuple', {
   // Prevents the property from being listed by Object.getOwnPropertyNames
   enumerable: false,
   value() {
-    return this.then(__tupleResolve, __tupleReject)
+    return this.then(tupleResolve, tupleReject)
   }
 })
