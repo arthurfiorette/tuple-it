e232be88c018035079ce23cecb04083fee9bb0b7
Arthur Fiorette <me@arthur.place>
2024-05-07 00:24:56 -0300

    docs: improved jsdocs

--- index.d.ts
+++ index.d.ts
@@ -1,37 +1,29 @@
 /**
- * TupleResult is a tuple type that represents the result of a promise.
+ * TupleResult represents the result of a promise as a tuple.
  *
- * If the promise is rejected, the tuple will contain an Error object. If the promise is resolved, the tuple will
- * contain null as the first element and the result as the second element.
+ * - If the promise is rejected, the tuple will contain an Error object.
+ * - If the promise is resolved, the tuple will contain null as the first element and the result as the second element.
  */
 export type TupleResult<T> = [Error] | [null, T]
 
 /**
  * Returns a promise that resolves to a tuple containing the result of the promise.
  *
- * @example
- *
- * ```ts
- * const [error, result] = await t(promise);
- * ```
+ * @example Const [error, result] = await t(promise);
  */
 export declare function tuple<T>(promise: Promise<T>): Promise<TupleResult<T>>
 
 /**
- * Returns a promise that resolves to a t containing the result of the promise.
+ * Returns a promise that resolves to a tuple containing the result of the promise.
  *
- * @example
- *
- * ```ts
- * const [error, result] = await t(promise);
- * ```
+ * @example Const [error, result] = await t(promise);
  */
 export declare function tuple<T>(
   promise: PromiseLike<T>
 ): PromiseLike<TupleResult<T>>
 
 /**
- * Simply helper when functions may not return a promise.
+ * Helper function for cases where functions may not return a promise.
  *
  * @example
  *
@@ -45,13 +37,13 @@ export declare function tuple<T>(
 export declare function tuple<T>(notPromise: T | PromiseLike<T>): TupleResult<T>
 
 /**
- * Transforms the promise into a tuple containing the result of the promise.
+ * Transforms a promise into a tuple containing the result of the promise.
  *
- * Simple type alias for {@linkcode tuple}
+ * Alias for {@linkcode tuple}.
  */
 export declare const t: typeof tuple
 
-/** Error thrown when a rejected promise is not instance of Error. */
+/** Error thrown when a rejected promise is not an instance of {@linkcode Error}. */
 export declare class TupleItError<E = unknown> extends Error {
   /** The error that caused the promise to be rejected. */
   error: E
